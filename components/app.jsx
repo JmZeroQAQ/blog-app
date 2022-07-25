3fad0b2ecb3124434246e5a77b390e7aca65c906
Yu Xian <1932699628@qq.com>
2022-07-25 10:42:12 +0800

    create blog-app

---
+++ components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <h4>一个伟大的开始！！！</h4>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;
